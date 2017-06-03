bf25f15b79aed995b7788a4ce804af753023db97
menghu07 <812877317@qq.com>
2017-06-04 00:02:10 +0800

    edit apptest 111 222 333

--- MyBatis-Project/src/test/java/com/apeny/mybatisproject/AppTest.java
+++ MyBatis-Project/src/test/java/com/apeny/mybatisproject/AppTest.java
@@ -57,7 +57,7 @@ public class AppTest
 			SqlSession session = factory.openSession();
 			BlogMapper mapper = session.getMapper(BlogMapper.class);
 			Blog blog = mapper.selectBlog(102);
-			System.out.println("blog digest 111 222 " + blog.getDigest() + blog + "fff");
+			System.out.println("blog digest 111 222 333 " + blog.getDigest() + blog + "fff");
 			System.out.println(session.getConfiguration().getParameterMapNames());
 			System.out.println("session." + session.getConnection());
 		} catch (IOException e) {
