7661db72c59663fe20bb1bcc1055d1d02e92e380
menghu07 <812877317@qq.com>
2017-06-19 19:15:35 +0800

    为什么同样的版本不一样的egit

--- ServletJsp/src/test/java/com/apeny/servletjsp/aop/TestLogAspect.java
+++ ServletJsp/src/test/java/com/apeny/servletjsp/aop/TestLogAspect.java
@@ -1,10 +1,7 @@
 package com.apeny.servletjsp.aop;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-
 import com.apeny.servletjsp.spring.bean.ArithmeticCalculator;
-import com.apeny.servletjsp.spring.bean.ArithmeticCalculatorImpl;
 
 public class TestLogAspect {
     
@@ -19,13 +16,9 @@ public class TestLogAspect {
         //config/applicationContext.xml
         FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/config/applicationContext.xml");
         ArithmeticCalculator calculator = context.getBean(ArithmeticCalculator.class);
-//        ArithmeticCalculatorImpl  impl = (ArithmeticCalculatorImpl) context.getBean("arithmeticCalculatorImpl");
-//        ArithmeticCalculatorImpl  impl2 = (ArithmeticCalculatorImpl) context.getBean(ArithmeticCalculatorImpl.class);
+        ArithmeticCalculator  impl = (ArithmeticCalculator) context.getBean("arithmeticCalculatorImpl");
         calculator.add(2, 3);
-//        impl.add(2, 90);
-//        impl.toString();
-         context.close();
-        
-//        context.close();
+        impl.add(9, 293);
+        context.close();
     }
 }
