4df9e94aa15b6e69b0cc3fe5959acf5f878169d4
menghu07 <812877317@qq.com>
2017-06-20 00:50:10 +0800

    throw exception test

--- ServletJsp/src/main/java/com/apeny/servletjsp/spring/bean/ArithmeticCalculatorImpl.java
+++ ServletJsp/src/main/java/com/apeny/servletjsp/spring/bean/ArithmeticCalculatorImpl.java
@@ -7,6 +7,9 @@ public class ArithmeticCalculatorImpl implements ArithmeticCalculator  {
 
     public int add(int i, int j) {
         System.out.println("i = " + i + ", j = " + j + " === " + (i + j) );
+        if (i < 10) {
+        	throw new IllegalArgumentException();
+        }
         return i + j;
     }
 
