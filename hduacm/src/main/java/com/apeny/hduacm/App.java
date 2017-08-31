70c6785cdd5258570b36b9a1d8f9e3499b4497c1
menghu07 <812877317@qq.com>
2017-09-01 07:30:38 +0800

    杭电acm

---
+++ hduacm/src/main/java/com/apeny/hduacm/App.java
@@ -0,0 +1,13 @@
+package com.apeny.hduacm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
