d03afaccf947d2828bd69ab80f024648302184c1
menghu07 <812877317@qq.com>
2017-09-11 23:39:06 +0800

    Filter around

---
+++ ServletJsp/src/main/java/com/apeny/servletjsp/filter/AroundFilter.java
@@ -0,0 +1,29 @@
+package com.apeny.servletjsp.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+/**
+ * Created by ahu on 2017年09月11日.
+ */
+public class AroundFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init拦截自动跳转页面");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("before request....");
+        chain.doFilter(request, response);
+        System.out.println("after request....");
+        PipedInputStream pipedInputStream = new PipedInputStream(new PipedOutputStream());
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy拦截自动跳转页面");
+    }
+}
