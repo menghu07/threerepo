bf56ed1fee51f77cc04f7a0889df5693e43a4a19
menghu07 <812877317@qq.com>
2017-09-18 15:37:38 +0800

    mybatis....

--- Miscellaneous/src/test/java/com.apeny.thread/mchar/CharMain.java
+++ Miscellaneous/src/test/java/com.apeny.thread/mchar/CharMain.java
@@ -10,6 +10,8 @@ public class CharMain {
     public static void main(String[] args) {
         char c = '值';
         System.out.println("sm c: 中文" + c);
+
+        sun.misc.Unsafe f = null;
         Calendar cc = Calendar.getInstance();
         cc.set(1960, 0, 1);
         System.out.println(new Date());
