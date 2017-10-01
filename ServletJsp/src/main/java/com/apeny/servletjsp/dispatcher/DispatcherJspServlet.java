01afbe3cf62160b13c2ea90af2b225558e0decc2
menghu07 <812877317@qq.com>
2017-10-01 19:46:57 +0800

    response.getWrite()和response.getOutputStream()不能同时使用

--- ServletJsp/src/main/java/com/apeny/servletjsp/dispatcher/DispatcherJspServlet.java
+++ ServletJsp/src/main/java/com/apeny/servletjsp/dispatcher/DispatcherJspServlet.java
@@ -1,31 +1,34 @@
 package com.apeny.servletjsp.dispatcher;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 /**
  * 1. forward 之前执行flush会引起forwar执行抛出异常，PrintWrite.flush()会提交Response，但还是可以在flush的
+ * 2. !!! response.getWriter()和response.getOutputStream()不能同时用
+ * 3 在调试时，默认是useOutputStream=false,偶然出现问题啦。。。。，调试时报getOutputStream()已经用了
  */
-@WebServlet(name = "dispatcherservlet", value="*.do")
+@WebServlet(name = "dispatcherservlet", value = "*.do")
 public class DispatcherJspServlet extends HttpServlet {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6230022484059652855L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6230022484059652855L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String servletPath = req.getServletPath();
-		servletPath = servletPath.replace(".do", ".jsp");
-		req.setAttribute("ActionLabel", "new Action");
-		//forward验证原理
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String servletPath = req.getServletPath();
+        servletPath = servletPath.replace(".do", ".jsp");
+        req.setAttribute("ActionLabel", "new Action");
+        //forward验证原理
 //        PrintWriter pw = resp.getWriter();
 //        pw.write("the response from resp: " + " " + req.getServletPath());
 //        pw.flush();
@@ -50,12 +53,15 @@ public class DispatcherJspServlet extends HttpServlet {
         resp.flushBuffer();
         PrintWriter pwAfterInclude = resp.getWriter();
         pwAfterInclude.write("<br/> you can write still write...");
-	}
+//!!!       ServletOutputStream outputStream = resp.getOutputStream();
+//!!!    outputStream.println("<br/>会冲突吗");
+//!!!        outputStream.flush();
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String servletPath = req.getServletPath();
-		servletPath = servletPath.replace(".do", ".jsp");
-		req.getRequestDispatcher("/WEB-INF" + servletPath).forward(req, resp);
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String servletPath = req.getServletPath();
+        servletPath = servletPath.replace(".do", ".jsp");
+        req.getRequestDispatcher("/WEB-INF" + servletPath).forward(req, resp);
+    }
 }
