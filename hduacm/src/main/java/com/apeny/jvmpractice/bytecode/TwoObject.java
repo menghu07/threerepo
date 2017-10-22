18e3369e27fbca8f1eb6e456b06d9bb0bea48927
apeny <812877318@qq.com>
2017-10-23 00:15:17 +0800

    java class 反编译 -p -v

--- hduacm/src/main/java/com/apeny/jvmpractice/bytecode/TwoObject.java
+++ hduacm/src/main/java/com/apeny/jvmpractice/bytecode/TwoObject.java
@@ -15,14 +15,30 @@ public class TwoObject {
     private long i;
     long jlong;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         TwoObject twoObject = new TwoObject(23);
         TwoObject ijTwoObject = new TwoObject(23, 35);
+        ijTwoObject.fun1(10, Float.NaN);
         Object object = new Object();
     }
 
-    public int fun1(double dbl) throws Exception {
+    public int fun1(double dbl, float fi) throws Exception {
         System.out.print("you");
+        short f1 = 9;
+        byte f2 = 10;
+        System.out.println(f1 > f2);
+        if (f1 < f2) {
+            int f4 = 102;
+            f4 = 20;
+        } else {
+            f2 = 127;
+        }
+
+        float f3 = 20;
+        float f4 = Float.NaN;
+        //做反方向判断fcmpg
+        System.out.println(f3 != f4);
+        int[] arr = new int[100];
         return 1;
     }
 }
